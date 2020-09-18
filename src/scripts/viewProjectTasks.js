4088f5734667c54f9feafda2d5f0a9b411b32075
psialedev <psialedev@gmail.com>
2020-09-18 12:30:42 -0500

    installed date-fns to format dates, and created dateParser function

--- src/scripts/viewProjectTasks.js
+++ src/scripts/viewProjectTasks.js
@@ -1,18 +1,34 @@
+import { format } from 'date-fns';
+
 import * as handleInput from './handlingUserInput';
 import * as localStorage from './localStorage';
 
 
 const setDate = (id) => { handleInput.setTaskProperty('dateInput', id, 'dueDate'); };
 
+const dateParser = (task) => {
+const initialDate = new Date(task);
+const day =   initialDate.getDate();
+const month = initialDate.getMonth();
+const year =  initialDate.getFullYear();
+const dayFormatted = format(day, "do");
+const monthFormatted = format(month, "LLL");
+  return `${dayFormatted} ${monthFormatted}, ${year}`;  
+}
 
 const getDate = (id) => {
   const project = localStorage.retrieveItem('project');
-  const task = project.items[id].dueDate;
-  if (!task) {
-    return 'Set a date';
+  if (id && project.items[id]) { 
+    const task = project.items[id].dueDate;
+    if (!task) {
+      return 'Set a date';
+    }
+    console.log(dateParser(task))
+    return `${task}`;
   }
-  return `${task}`;
 };
+
+
 
 
 export { setDate, getDate };
