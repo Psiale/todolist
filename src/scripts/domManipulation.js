285f606f6fc9b98aaa5a6f0cd4b0fdb2c687b3da
Rory Hellier <rory.hellier@googlemail.com>
2020-09-29 23:56:42 +0100

    Created todoListItemGenerator script and added exports in the file and imports into domManip

--- src/scripts/domManipulation.js
+++ src/scripts/domManipulation.js
@@ -114,29 +114,6 @@ const todoListMainContainer = () => {
 
 const projectSelectorList = () => {
   const mainContainer = generator.htmlGenerator('div', 'project-selector-container', 'projectSelectorContainer');
-
-  //const todoListItemGenerator = (arr = []) => {
-  //  const todoListArrContainer = generator.htmlGenerator('div', 'project-todoList-arr-container');
-  //  for (let i = 0; i < arr.length; i++) {
-  //    const element = arr[i];
-  //    const todoListItemContainer = generator.htmlGenerator('div', 'todolist-item-container', `todoListItemContainer${i}`);
-  //    const todoListItemDeleteButton = generator.htmlGenerator('button', 'todolist-item-delete-btn', `todoListItemDeleteBtn${i}`);
-  //    const todoListItemDeleteButtonText = generator.textGenerator('p', '<i class="fas fa-times"></i>');
-  //    todoListItemDeleteButton.append(todoListItemDeleteButtonText);
-  //    const textContent = generator.textGenerator('p', `${element.projectTitle}`);
-  //    textContent.addEventListener('click', () => {
-  //      hui.getIdFromProject(i);
-  //      generator.reload();
-  //    });
-  //    todoListItemDeleteButton.addEventListener('click', () => {
-  //      hui.obliterateProject(i);
-  //      generator.reload();
-  //    });
-  //    todoListItemContainer.append(textContent, todoListItemDeleteButton);
-  //    todoListArrContainer.appendChild(todoListItemContainer);
-  //  }
-  //  return todoListArrContainer;
-  //};
   const backgroundContainer = generator.htmlGenerator('div', 'todoList-background-container');
   mainContainer.append(todoListItemGenerator(hui.projectArr), backgroundContainer);
   return mainContainer;
